b4ae6679ac9c2f56ae94efdf04600693a6489562
tomyleca <tomyleca14@gmail.com>
2025-04-08 22:22:24 +0000

    archivos

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -4,6 +4,6 @@ int main(int argc, char* argv[]) {
     
     //saludar("kernel");
     archivo_config= "../kernel/kernel.config";
-    leer_archivo_config(archivo_config);
+    //leer_archivo_config(archivo_config);
     return 0;
 }
--- kernel/src/kernel.h
+++ kernel/src/kernel.h
@@ -7,19 +7,10 @@
 #include<commons/string.h>
 #include<commons/config.h>
 #include<readline/readline.h>
-#include "globales.h"
-#include "cliente.h"
+#include "globalesKernel.h"
 
-int socket_kernel_memoria;
-char* archivo_config;
-char* ip_memoria;
-char* algoritmo_planificacion;
-int puerto_memoria;
-int tiempo_suspension;
-char* log_level;
 
-t_log* logger_kernel;
-t_config* config_kernel;
+
 
 
 #endif
