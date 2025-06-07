3e631b1e65aecba6b1d4346d5938b0426d0ed065
Benjamin Alex Alegre <81981001+BenjaminAlegre@users.noreply.github.com>
2025-06-06 22:38:02 -0300

    Prueba funciones proceso memoria

--- kernel/src/conexionConMemoria.c
+++ kernel/src/conexionConMemoria.c
@@ -14,3 +14,48 @@ void cerrar_conexion_memoria(int socket) {
         liberar_conexion(socket);
     }
 }
+
+bool inicializar_proceso_memoria(uint32_t pid, uint32_t tam) {
+    int socket_memoria = crear_conexion_memoria();
+    if (socket_memoria == -1) return false;
+
+    t_paquete* paquete = crear_super_paquete(INICIALIZAR_PROCESO);
+    if (paquete == NULL) {
+        log_error(loggerKernel, "Error al crear paquete para inicializar proceso");
+        cerrar_conexion_memoria(socket_memoria);
+        return false;
+    }
+
+    cargar_uint32_t_al_super_paquete(paquete, pid);
+    cargar_uint32_t_al_super_paquete(paquete, tam);
+    
+    enviar_paquete(paquete, socket_memoria);
+    eliminar_paquete(paquete);
+
+    op_code_memoria respuesta = recibir_operacion(socket_memoria);
+    cerrar_conexion_memoria(socket_memoria);
+
+    return respuesta == INICIALIZAR_PROCESO;
+}
+
+bool suspender_proceso_memoria(uint32_t pid) {
+    int socket_memoria = crear_conexion_memoria();
+    if (socket_memoria == -1) return false;
+
+    t_paquete* paquete = crear_super_paquete(SUSPENDER_PROCESO);
+    if (paquete == NULL) {
+        log_error(loggerKernel, "Error al crear paquete para suspender proceso");
+        cerrar_conexion_memoria(socket_memoria);
+        return false;
+    }
+
+    cargar_uint32_t_al_super_paquete(paquete, pid);
+    enviar_paquete(paquete, socket_memoria);
+    eliminar_paquete(paquete);
+
+    op_code_memoria respuesta = recibir_operacion(socket_memoria);
+    cerrar_conexion_memoria(socket_memoria);
+
+    return respuesta == SUSPENDER_PROCESO;
+}
+
--- kernel/src/conexionConMemoria.h
+++ kernel/src/conexionConMemoria.h
@@ -7,6 +7,21 @@
 #include "globalesKernel.h"
 #include "../../utils/src/utils/shared.h"
 #include "../../utils/src/utils/conexiones.h"
+
+// Operaciones de memoria
+typedef enum {
+    INICIALIZAR_PROCESO,
+    SUSPENDER_PROCESO,
+    DES_SUSPENDER_PROCESO,
+    FINALIZAR_PROCESO,
+    DUMP_MEMORY,
+    DUMP_MEMORY_OK,
+    DUMP_MEMORY_ERROR
+} op_code_memoria;
+
+// Funciones de conexión con memoria
 int crear_conexion_memoria();
 void cerrar_conexion_memoria(int socket);
+bool inicializar_proceso_memoria(uint32_t pid, uint32_t tam);
+bool suspender_proceso_memoria(uint32_t pid);
 #endif 
